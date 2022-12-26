8d13668035a9839cc5d876a94b3fef06a97794ee
Chí Hiếu <hieumeo171@gmail.com>
2022-12-26 23:49:24 +0700

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Gomoku.GameOverChecking;
+
+namespace Gomoku
+{
+    public partial class Form1 : Form
+    {
+        public static int numOfRows = 20;
+        private static int tileSize = 32;
+
+        private static int WIDTH = numOfRows * tileSize + tileSize * 8;
+        private static int HEIGHT = numOfRows * tileSize + tileSize * 2;
+
+        public static int[,] board = new int[numOfRows, numOfRows];
+        private static Button[,] btnList = new Button[numOfRows, numOfRows];
+
+        private const int NONE = 0;
+        private const int WHITE = 1;
+        private const int BLACK = 2;
+        public static int currentTurn = WHITE;
+
+        private static Image whiteSymbol = Image.FromFile(Path.GetFullPath("Materials\\X.png"));
+        private static Image blackSymbol = Image.FromFile(Path.GetFullPath("Materials\\O.png"));
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitBoard();
+            InitGUI();
+            this.ClientSize = new Size(WIDTH, HEIGHT);
+        }
+
+        private void InitBoard()
+        {
+            for (int i = 0; i < numOfRows; i++)
+            {
+                for (int j = 0; j < numOfRows; j++)
+                {
+                    Button btn = new Button();
+                    btn.Location = new Point(tileSize + tileSize * j, tileSize + tileSize * i);
+                    btn.Width = tileSize;
+                    btn.Height = tileSize;
+                    btn.Click += btn_Click;
+
+                    board[j, i] = NONE;
+                    btnList[j, i] = btn;
+                    this.Controls.Add(btn);
+                }
+            }
+        }
+
+        private void InitGUI()
+        {
+            Panel panel = new Panel();
+            panel.Location = new Point(WIDTH - tileSize * 6, tileSize);
+            panel.Size = new Size(tileSize * 6, HEIGHT - tileSize * 2);
+            panel.BackColor = Color.Silver;
+            panel.Visible = true;
+
+            Label player1 = new Label();
+            player1.Location = new Point(WIDTH - tileSize * 6 + tileSize / 2, tileSize + tileSize / 2);
+            // player1.Size = new Size(tileSize, tileSize / 2);
+            player1.Text = "X";
+            
+            Label player2 = new Label();
+            player2.Location = new Point(WIDTH - tileSize * 5 + tileSize / 2, tileSize + tileSize / 2);
+            // player2.Size = new Size(tileSize, tileSize / 2);
+            player2.Text = "O";
+            
+            Controls.Add(player1);
+            Controls.Add(player2);
+            
+            this.Controls.Add(panel);
+        }
+
+        private void ClearBoard()
+        {
+            for (int i = 0; i < numOfRows; i++)
+            {
+                for (int j = 0; j < numOfRows; j++)
+                {
+                    // btnList[i, j].Text = "";
+                    btnList[i, j].Image = null;
+                    board[i, j] = NONE;
+                    currentTurn = WHITE;
+                }
+            }
+        }
+
+        void btn_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (btn.Text != "") return;
+            // btn.Text = currentTurn == WHITE ? "X" : "O";
+            btn.Image = currentTurn == WHITE ? whiteSymbol : blackSymbol;
+
+            int j = (btn.Location.X - tileSize) / tileSize;
+            int i = (btn.Location.Y - tileSize) / tileSize;
+
+            board[j, i] = currentTurn;
+
+            if (IsWon())
+            {
+                MessageBox.Show("Player " + (currentTurn == WHITE ? "1" : "2") + " wins");
+                ClearBoard();
+                return;
+            }
+
+            currentTurn = 3 - currentTurn;
+        }
+
+
+        
+    }
+}
---
+++ GameOverChecking.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using static Gomoku.Form1;
+
+namespace Gomoku
+{
+    public class GameOverChecking
+    {
+        public static bool IsWon()
+        {
+            int answer = Math.Max(CheckNgangDoc(), CheckCheo());
+
+            //MessageBox.Show(CheckNgangDoc() + " " + CheckCheo() + " " + answer.ToString() + " " + currentTurn);
+
+            return answer == 5;
+        }
+
+        private static int CheckNgangDoc()
+        {
+            int answer = 0;
+
+            int[,] left = new int[numOfRows, numOfRows];
+            int[,] right = new int[numOfRows, numOfRows];
+            int[,] up = new int[numOfRows, numOfRows];
+            int[,] down = new int[numOfRows, numOfRows];
+
+            for (int i = 0; i < numOfRows; i++)
+            {
+                left[i, numOfRows - 1] = (board[i, numOfRows - 1] == currentTurn ? 1 : 0);
+                right[i, 0] = (board[i, 0] == currentTurn ? 1 : 0);
+                up[0, i] = (board[0, i] == currentTurn ? 1 : 0);
+                down[numOfRows - 1, i] = (board[numOfRows - 1, i] == currentTurn ? 1 : 0);
+            }
+
+            for (int i = 0; i < numOfRows; i++)
+            {
+                for (int j = 0; j < numOfRows; j++)
+                {
+                    if (board[i, j] == currentTurn && j > 0) left[i, j] = left[i, j - 1] + 1;
+                    else left[i, j] = 0;
+
+                    if (board[j, i] == currentTurn && j > 0) up[j, i] = up[j - 1, i] + 1;
+                    else up[j, i] = 0;
+
+                    j = numOfRows - 1 - j;
+
+                    if (board[i, j] == currentTurn && j < numOfRows-1) right[i, j] = right[i, j + 1] + 1;
+                    else right[i, j] = 0;
+
+                    if (board[j, i] == currentTurn && j < numOfRows-1) down[j, i] = down[j + 1, i] + 1;
+                    else down[j, i] = 0;
+
+                    j = numOfRows - 1 - j;
+                }
+            }
+            for (int i = 0; i < numOfRows; i++)
+            {
+                for (int j = 0; j < numOfRows; j++)
+                {
+                    int value = Math.Max(Math.Max(left[i, j], right[i, j]), Math.Max(up[i, j], down[i, j]));
+                    if (value > answer)
+                    {
+                        answer = value;
+                    }
+                }
+            }
+            return answer;
+        }
+
+        private static int CheckCheo()
+        {
+            int answer = 0;
+
+            int[,] top_left = new int[numOfRows, numOfRows];
+            int[,] bot_left = new int[numOfRows, numOfRows];
+            int[,] top_right = new int[numOfRows, numOfRows];
+            int[,] bot_right = new int[numOfRows, numOfRows];
+
+            for (int i = 0; i < numOfRows; i++)
+            {
+                top_left[i, 0] = board[i, 0] == currentTurn ? 1 : 0;
+                top_left[0, i] = board[0, i] == currentTurn ? 1 : 0;
+
+                bot_left[i, 0] = board[i, 0] == currentTurn ? 1 : 0;
+                bot_left[numOfRows - 1, i] = board[numOfRows - 1, i] == currentTurn ? 1 : 0;
+
+                top_right[0, i] = board[0, i] == currentTurn ? 1 : 0;
+                top_right[i, numOfRows - 1] = board[i, numOfRows - 1] == currentTurn ? 1 : 0;
+
+                bot_right[i, numOfRows - 1] = board[i, numOfRows - 1] == currentTurn ? 1 : 0;
+                bot_right[numOfRows - 1, i] = board[numOfRows - 1, i] == currentTurn ? 1 : 0;
+            }
+
+
+            for (int i = 0; i < numOfRows; i++)
+            {
+                for (int j = 0; j < numOfRows; j++)
+                {
+                    if (board[i,j] == currentTurn && i > 0 && j > 0)
+                    {
+                        top_left[i,j] = top_left[i - 1,j - 1] + 1;
+                    }
+                    else top_left[i,j] = 0;
+
+                    if (board[j,i] == currentTurn && j < numOfRows-1 && i > 0)
+                    {
+                        bot_left[j,i] = bot_left[j + 1,i - 1] + 1;
+                    }
+                    else bot_left[j,i] = 0;
+
+                    j = numOfRows - 1 - j;
+
+                    if (board[j,i] == currentTurn && j < numOfRows-1 && i < numOfRows-1)
+                    {
+                        bot_right[j,i] = bot_right[j + 1,i + 1] + 1;
+                    }
+                    else bot_right[j,i] = 0;
+
+                    if (board[i,j] == currentTurn && i > 0 && j < numOfRows-1)
+                    {
+                        top_right[i,j] = top_right[i - 1,j + 1] + 1;
+                    }
+                    else top_right[i,j] = 0;
+
+                    j = numOfRows - 1 - j;
+                }
+            }
+            for (int i = 0; i < numOfRows; i++)
+            {
+                for (int j = 0; j < numOfRows; j++)
+                {
+                    int value = Math.Max(Math.Max(top_left[i,j], bot_right[i,j]), Math.Max(top_right[i,j], bot_left[i,j]));
+                    if (value > answer)
+                    {
+                        answer = value;
+                    }
+                }
+            }
+            return answer; 
+        }
+    }
+}
